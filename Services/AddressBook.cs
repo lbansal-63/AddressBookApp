9a9d13c608fb7f0cc373f1157cb2755d26353b7f
Laksha Bansal <laksha.bansal7015@gmail.com>
2026-09-14 12:34:09 +0530

    Implemented Use Case 3

---
+++ Services/AddressBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AddressBookApp.Models;
+using AddressBookApp.Validation;
+
+namespace AddressBookApp.Services
+{
+    public class AddressBook
+    {
+        private List<Contact> contacts = new();
+
+        public IReadOnlyList<Contact> Contacts => contacts;
+
+        public void AddContact(Contact contact)
+        {
+            ContactValidator.Validate(contact);
+
+            contacts.Add(contact);
+
+            Console.WriteLine("Contact added successfully.");
+        }
+
+        public void PrintAll()
+        {
+            if (contacts.Count == 0)
+            {
+                Console.WriteLine("No contacts found.");
+                return;
+            }
+
+            foreach (Contact contact in contacts)
+            {
+                Console.WriteLine(contact);
+            }
+        }
+    }
+}
