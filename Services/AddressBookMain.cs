520a05200af5bdcacafb57b36501cf3abd16973a
Laksha Bansal <laksha.bansal7015@gmail.com>
2026-09-15 10:01:58 +0530

    Count Contacts

---
+++ Services/AddressBookMain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq; 
+
+namespace AddressBookApp.Services
+{
+    public class AddressBookMain
+    {
+        private List<AddressBook> books = new(); 
+
+        public void AddAddressBook(AddressBook book)
+        {
+            books.Add(book); 
+        }
+
+        public int GetTotalContactCount()
+        {
+            return books.Sum(b=>b.Contacts.Count);
+        }
+
+        public List<AddressBook> GetAddressBooks()
+        {
+            return books; 
+        }
+
+    }
+}
